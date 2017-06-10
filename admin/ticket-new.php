062811e7784f8ad663592319663fe7afc1b8401a
websupporter <webmaster@websupporter.net>
2017-06-10 08:23:34 +0300

    code cleanup ticket-new.php

--- admin/ticket-new.php
+++ admin/ticket-new.php
@@ -1,16 +1,19 @@
 <?php
-	$fields = sts_get_create_ticket_form_fields( 'admin' );
-	if( isset( $_GET['ticket-created'] ) ):
-		?>
-		<script>location.href="?page=sts&action=single&ticket-new=1&ID=<?php echo $_GET['ticket-id']; ?>";</script>
-		<?php
-		die();
-	endif;
+$fields = sts_get_create_ticket_form_fields( 'admin' );
+if ( isset( $_GET['ticket-created'] ) ) : // Input var okay.
+	$ticket_id = ( isset( $_GET['ticket-id'] ) ) ? sanitize_text_field( wp_unslash( $_GET['ticket-id'] ) ) : 0; // Input var okay.
+	?>
+	<script>
+		location.href="?page=sts&action=single&ticket-new=1&ID=<?php echo (int) $ticket_id; ?>";
+	</script>
+	<?php
+	exit;
+endif;
 ?>
 <div id="sts-wrap" class="wrap">
 	<h2>
-		<img src="<?php echo STS_URL; ?>assetts/logo-small.svg" height="25px" />
-		<?php _e( 'Create a new ticket', 'sts' ); ?>
+		<img src="<?php echo esc_url( STS_URL ); ?>assetts/logo-small.svg" height="25px" />
+		<?php esc_html_e( 'Create a new ticket', 'sts' ); ?>
 	</h2>
 	<form method="post" class="ticket create" enctype="multipart/form-data">
 		<?php if( isset( $_SESSION['tickets']['error'] ) && is_wp_error( $_SESSION['tickets']['error'] ) ): ?>
@@ -20,12 +23,14 @@
 		<input type="hidden" name="t-action" value="ticket-create" />
 		<?php wp_nonce_field( 'ticket-create', 't-nonce' ); ?>
 
-		<?php foreach( $fields as $field )
+		<?php
+		foreach ( $fields as $field ) {
 			sts_render_form_field( $field, 'admin' );
+		}
 		?>
 		<section>
 			<label>&ensp;</label>
-			<button class="button button-primary button-large"><?php _e( 'Send', 'sts' ); ?></button>
+			<button class="button button-primary button-large"><?php esc_html_e( 'Send', 'sts' ); ?></button>
 		</section>
 	</form>
 </div>
